ba0acf1ac16c0792a0c1e2fcb897c44d7d8adeba
Frida Johannesson <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2025-01-10 23:55:19 +0100

    Added hamburger menu and background.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function sayHello() {
-    alert('Hej från extern JavaScript!');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navList = document.querySelector('.nav-list');
+
+    hamburger.addEventListener('click', function() {
+        navList.classList.toggle('active');
+    });
+});
+
