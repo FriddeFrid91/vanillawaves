3d94ec0c98f13971387aa9ed29dbddaa7f77b0d3
Frida Johannesson <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2025-01-14 22:41:25 +0100

    Changed the background and edited the nav menu.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth;
+const height = window.innerHeight;
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+console.log(`Skärmens storlek är ${screenWidth}px x ${screenHeight}px`);
+
+
+console.log(`Webbläsarfönstrets storlek är ${width}px x ${height}px`);
     const hamburger = document.querySelector('.hamburger');
     const navList = document.querySelector('.nav-list');
 
